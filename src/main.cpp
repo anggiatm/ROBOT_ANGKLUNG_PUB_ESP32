2cb00bf86962882ad5ff7ce00d63dee93ea753aa
anggiatm <anggiat17@gmail.com>
2022-11-05 13:38:05 +0700

    add UI oled display

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,11 @@
-// #include <WiFi.h>
 #include <Arduino.h>
 #include <ESP32Servo.h>
-// #include <PubSubClient.h>
 
-// WiFiClient   wifiClient;
-// #define MQTT_BROKER "192.168.1.7"
-// #define MQTT_PORT 1883
-// #define SSID "wifi_rumah"
-// #define PASS "mora170894"
-// #define ID "ROBOT_PUBLISHER"
-
-// IPAddress IP = {192,168,1,7};
-// PubSubClient MQTTclient( IP, MQTT_PORT, wifiClient );
-// SemaphoreHandle_t sema_MQTT_KeepAlive;
-
-#include <Wire.h>               // Only needed for Arduino 1.6.5 and earlier
-#include "SSD1306Wire.h"        // legacy: #include "SSD1306.h"
-// OR #include "SH1106Wire.h"   // legacy: #include "SH1106.h"
-
-// For a connection via I2C using brzo_i2c (must be installed) include:
-// #include <brzo_i2c.h>        // Only needed for Arduino 1.6.5 and earlier
-// #include "SSD1306Brzo.h"
-// OR #include "SH1106Brzo.h"
-
-// For a connection via SPI include:
-// #include <SPI.h>             // Only needed for Arduino 1.6.5 and earlier
-// #include "SSD1306Spi.h"
-// OR #include "SH1106SPi.h"
-
-// Initialize the OLED display using Arduino Wire:
-SSD1306Wire display(0x3c, SDA, SCL);   // ADDRESS, SDA, SCL  -  SDA and SCL usually populate automatically based on your board's pins_arduino.h e.g. https://github.com/esp8266/Arduino/blob/master/variants/nodemcu/pins_arduino.h
-// SSD1306Wire display(0x3c, D3, D5);  // ADDRESS, SDA, SCL  -  If not, they can be specified manually.
-// SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);  // ADDRESS, SDA, SCL, OLEDDISPLAY_GEOMETRY  -  Extra param required for 128x32 displays.
-// SH1106Wire display(0x3c, SDA, SCL);     // ADDRESS, SDA, SCL
-
-// Initialize the OLED display using brzo_i2c:
-// SSD1306Brzo display(0x3c, D3, D5);  // ADDRESS, SDA, SCL
-// or
-// SH1106Brzo display(0x3c, D3, D5);   // ADDRESS, SDA, SCL
-
-// Initialize the OLED display using SPI:
-// D5 -> CLK
-// D7 -> MOSI (DOUT)
-// D0 -> RES
-// D2 -> DC
-// D8 -> CS
-// SSD1306Spi display(D0, D2, D8);  // RES, DC, CS
-// or
-// SH1106Spi display(D0, D2);       // RES, DC
+#include <U8g2lib.h>
+#include <Wire.h>
 
 
-#define DEMO_DURATION 3000
-typedef void (*Demo)(void);
+bool MIDI_MONITOR = false;
 
 int demoMode = 0;
 int counter = 1;
@@ -72,7 +26,6 @@ byte cc_type2;
 byte cc_val1;
 byte cc_val2;
 
-Servo myservo;
 
 class Channel {
   private:
@@ -119,7 +72,10 @@ String DISPLAY_DATA[5][3]= {
                   {"","",""},
                   {"","",""}};
 
-Servo CHANNEL[] = {myservo};
+const String MENU[] = {"Monitor", "Pin Config", "About"};
+uint8_t SELECTED = 0;
+
+// Servo CHANNEL[] = {myservo};
 
 const String NOTES[] = {"C-2", "C#-2", "D-2","D#-2", "E-2", "F-2", "F#-2", "G-2", "G#-2", "A-2", "A#-2", "B-2",
                         "C-1", "C#-1", "D-1","D#-1", "E-1", "F-1", "F#-1", "G-1", "G#-1", "A-1", "A#-1", "B-1",
@@ -138,48 +94,61 @@ int velocityToPwm(int velocity){
 }
 
 void execute(int channel, int velocity){
-  CHANNEL[0].write(velocityToPwm(velocity));
+  // myservo.write(velocityToPwm(velocity));
 }
 
-void drawFontFaceDemo() {
-  display.setTextAlignment(TEXT_ALIGN_LEFT);
-  display.setFont(ArialMT_Plain_10);
-  display.drawString(0, 0, "Hello world");
-  display.setFont(ArialMT_Plain_16);
-  display.drawString(0, 10, "Hello world");
-  display.setFont(ArialMT_Plain_24);
-  display.drawString(0, 26, "Hello world");
-}
 
-void drawRectDemo() {
-  // Draw a pixel at given position
-  for (int i = 0; i < 10; i++) {
-    display.setPixel(i, i);
-    display.setPixel(10 - i, i);
-  }
-  display.drawRect(12, 12, 20, 20);
-
-  // Fill the rectangle
-  display.fillRect(14, 14, 17, 17);
-
-  // Draw a line horizontally
-  display.drawHorizontalLine(0, 40, 20);
-
-  // Draw a line horizontally
-  display.drawVerticalLine(40, 0, 20);
-}
-
-void drawProgressBarDemo() {
-  int progress = (counter / 5) % 100;
-  // draw the progress bar
-  display.drawProgressBar(0, 32, 120, 10, progress);
-
-  // draw the percentage as String
-  display.setTextAlignment(TEXT_ALIGN_CENTER);
-  display.drawString(64, 15, String(progress) + "%");
-}
 int bytesToInt(int l_highByte, int l_lowByte) {
   return ((unsigned int)l_highByte << 8) + l_lowByte;
+}
+int currentState;
+int lastState = HIGH;
+void lcd_display( void *pvParameters){
+  // vTaskDelay(2000/portTICK_PERIOD_MS);
+  
+  pinMode(25, INPUT_PULLUP);
+  U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+  u8g2.begin();
+ 
+  for (;;){
+    currentState = digitalRead(25);
+    if(lastState == LOW && currentState == HIGH){
+      // MIDI_MONITOR = !MIDI_MONITOR;
+      SELECTED = SELECTED + 1;
+      if (SELECTED > 2){
+        SELECTED = 0;
+      }
+      
+    }
+    lastState = currentState;
+    
+    u8g2.clearBuffer();					// clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+    if(MIDI_MONITOR){
+      // u8g2.drawStr(15, 10, "LONELY BINARY");	// write something to the internal memory
+      u8g2.drawStr(10, 10, "Val");
+      u8g2.drawStr(40, 10, "Not");
+      u8g2.drawStr(70, 10, "Vel");
+      for(int data = 0; data<5; data++){
+          for(int row = 0; row<3; row++){
+            String a = String(DISPLAY_DATA[data][row]);
+             u8g2.drawStr(ROW_Y + (30 * row), ROW_X + (12 * (data+1)), a.c_str());
+          }
+        }
+    }
+    else {
+      u8g2.drawStr(20, 10, "MENU");
+      for(int i=0; i<3; i++){
+        if (i == SELECTED){
+          u8g2.drawStr(10,  10 * (i+2), ">");
+        }
+        u8g2.drawStr(20,  10 * (i+2), MENU[i].c_str());
+      }
+    }
+    
+    u8g2.sendBuffer();					// transfer internal memory to the display
+    // vTaskDelay(1000);
+  }
 }
 
 void broadcast_command( void *pvParameters ) {
@@ -189,14 +158,6 @@ void broadcast_command( void *pvParameters ) {
   for (;;){
     if (xQueueReceive( xQueue, &ReceivedValue, portMAX_DELAY ) == pdPASS) {
       count = count + 1;
-      display.clear();
-      display.setFont(ArialMT_Plain_10);
-      display.setTextAlignment(TEXT_ALIGN_LEFT);
-      display.drawString(10, 0, "Val");
-      display.drawString(40, 0, "Not");
-      display.drawString(70, 0, "Vel");
-
-      
       
       if (sizeof(DISPLAY_DATA) > 4){
         // int b[] = {count+1, count+5, count+5};
@@ -210,15 +171,6 @@ void broadcast_command( void *pvParameters ) {
         DISPLAY_DATA[0][2] = String(ReceivedValue[1]);
       }
 
-      for(int data = 0; data<5; data++){
-        for(int row = 0; row<3; row++){
-          display.drawString(ROW_Y + (30 * row), ROW_X + (10 * data), String(DISPLAY_DATA[data][row]));
-        }
-      }
-
-      // display.drawString(60, 50, String(count));
-      display.display();
-
       Serial.print(255);
       Serial.print(" ");
       Serial.print(ReceivedValue[0]);  
@@ -227,54 +179,35 @@ void broadcast_command( void *pvParameters ) {
       Serial.print(" ");
       Serial.println(lowByte(ReceivedValue[1]));
 
-      if (ReceivedValue[0] == 67){
-        
-      execute(0, ReceivedValue[1]);
+      if (ReceivedValue[0] == 47){
+        execute(0, ReceivedValue[1]);
       }
-
-
-      // command = String(ReceivedValue[0]) + " " + String(ReceivedValue[1]);
-
-      // xSemaphoreTake( sema_MQTT_KeepAlive, portMAX_DELAY );
-      // if ( MQTTclient.connected() )
-      // {
-      //   MQTTclient.publish( "tes", command.c_str());
-      // }
-      // xSemaphoreGive( sema_MQTT_KeepAlive );
     }
   }
   vTaskDelete ( NULL );
 }
 
 
-Demo demos[] = {drawFontFaceDemo, drawRectDemo, drawProgressBarDemo};
-int demoLength = (sizeof(demos) / sizeof(Demo));
+// Demo demos[] = {drawFontFaceDemo, drawRectDemo, drawProgressBarDemo};
+// int demoLength = (sizeof(demos) / sizeof(Demo));
 
 void setup() {
   Serial.begin(38400);
-  display.init();
 
-  display.flipScreenVertically();
-  display.setFont(ArialMT_Plain_10);
+  // display.init();
+  // display.flipScreenVertically();
+  // display.setFont(ArialMT_Plain_10);
+
 
   pinMode(LED_BUILTIN,OUTPUT);
-  ESP32PWM::allocateTimer(0);
-  ESP32PWM::allocateTimer(1);
-  ESP32PWM::allocateTimer(2);
-  ESP32PWM::allocateTimer(3);
-  myservo.setPeriodHertz(50);    // standard 50 hz servo
-	myservo.attach(servoPin1, 1000, 2000); // attaches the servo on pin 18 to the servo object
-  delay(500);
-  myservo.write(0);
-  delay(500);
-  // sema_MQTT_KeepAlive   = xSemaphoreCreateBinary();
-  // xSemaphoreGive( sema_MQTT_KeepAlive ); // found keep alive can mess with a publish, stop keep alive during publish
 
   xQueue = xQueueCreate(100, sizeof(int[2]));
   if(xQueue != NULL){
     // xTaskCreatePinnedToCore( MQTTkeepalive, "MQTTkeepalive", 1024*15, NULL, 3, NULL, 0 );
-    xTaskCreatePinnedToCore( broadcast_command, "broadcast_command", 1024*15, NULL, 4, NULL, 1);
+    xTaskCreatePinnedToCore(lcd_display, "lcd_display", 1024*6, NULL, 3, NULL, 1);
+    xTaskCreatePinnedToCore(broadcast_command, "broadcast_command", 1024*15, NULL, 4, NULL, 1);
   }
+  // vTaskDelete(NULL);
 }
 
 void loop() {
@@ -301,22 +234,6 @@ void loop() {
         int value = bytesToInt(cc_val1, cc_val2);
         int a[] = {control, value};
         xQueueSend( xQueue, &a, portMAX_DELAY);
-
-        // Serial.print(255);
-        // Serial.print(" ");
-        // Serial.print(control);  
-        // Serial.print(" ");
-        // Serial.print(highByte(value));
-        // Serial.print(" ");
-        // Serial.println(lowByte(value));
-        // if(value > 90 ){
-        //   digitalWrite(LED_BUILTIN, HIGH);
-        //   myservo.write(180);   
-        // }
-        // else {
-        //   digitalWrite(LED_BUILTIN, LOW);
-        //   myservo.write(0);
-        // }
     }
   }
 }
